fd1f292a52febc6a510f53eabba39693da14c890
victor.ludwick <victor.domingues@vm2.com.br>
2019-08-30 16:02:09 -0300

    finalizando o header

--- boot/design/src/js/main.js
+++ boot/design/src/js/main.js
@@ -1,4 +1,18 @@
 $(function(){
-  
+  $("#botao").on("click", function(event) {
+    $(this).toggleClass("active");
+  });
+  $("#linguagem").on("mouseenter", function(event) {
+    // alert("entrou");
+    // $(this).toggleClass("open");
+    // $(".dropdown-toggle").dropdown();
+    $("#teste").addClass("show");
+  });
+  $("#linguagem").on("mouseleave", function(event) {
+    // alert("saiu");
+    // $(this).toggleClass("open");
+    // $(".dropdown-toggle").dropdown();
+    $("#teste").removeClass("show");
+  });
 });
  
